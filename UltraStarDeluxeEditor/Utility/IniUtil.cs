2f42c1e67927a3a972a659bc7808aa20d06a672f
Nuhser <martinimmel@gmail.com>
2022-11-13 14:54:14 +0100

    Refactor IniUtil

--- UltraStarDeluxeEditor/Utility/IniUtil.cs
+++ UltraStarDeluxeEditor/Utility/IniUtil.cs
@@ -2,17 +2,18 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace UltraStarDeluxeEditor.Utility {
-    public class IniUtility {
+    public class IniUtil {
         [DllImport("kernel32", CharSet = CharSet.Unicode)]
         private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
 
         [DllImport("kernel32", CharSet = CharSet.Unicode)]
         private static extern int GetPrivateProfileString(string section, string key, string defaultValue,
-            StringBuilder retVal, int size, string filePath);
+            StringBuilder returnValue, int size, string filePath);
 
         public static string Read(string section, string key, string filePath) {
-            var retVal = new StringBuilder(255);
-            return GetPrivateProfileString(section, key, "", retVal, 255, filePath).ToString();
+            var returnValue = new StringBuilder(255);
+            GetPrivateProfileString(section, key, "", returnValue, 255, filePath);
+            return returnValue.ToString();
         }
 
         public static void Write(string section, string key, string value, string filePath) {
