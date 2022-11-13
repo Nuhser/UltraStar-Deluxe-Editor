523ef2456c0fd57c86ba1f1ab5f31349c0fe8ebf
Nuhser <martinimmel@gmail.com>
2022-11-13 14:13:53 +0100

    First commit

---
+++ UltraStarDeluxeEditor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UltraStarDeluxeEditor {
+    internal static class Program {
+        /**
+         * The main entry point for the application.
+         */
+        [STAThread]
+        private static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
